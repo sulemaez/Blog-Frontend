bc1be493259100b3199016fa225c7d6299a6cc44
sulemaez <steve.oyeyo@gmail.com>
2019-05-02 17:53:01 +0300

    added quill

---
+++ src/admin/components/AddPost.vue
@@ -0,0 +1,48 @@
+<template>
+     <div>
+        <quill-editor ref="myTextEditor"
+                v-model="content"
+                :config="editorOption">
+        </quill-editor>
+
+        <button @click="save" style="background-color:blue;">
+              Save
+        </button>
+     </div>
+</template>
+
+
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+export default {
+  name: 'app',
+  mounted(){
+      
+  },
+  data(){
+    return{
+      content : "",
+      editorOption: {}
+    }
+  },
+  methods:{
+     save(){
+        alert(this.editor.root.innerHTML)
+     }
+  },
+  components: {
+      quillEditor
+  },
+  computed:{
+     editor(){
+       return this.$refs.myTextEditor.quill
+     }
+  }
+}
+</script>
+
